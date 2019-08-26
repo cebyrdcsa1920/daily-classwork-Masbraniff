ff6789de73aa7f154729eec84c7664aa79b081eb
Masbraniff <masontyler272@gmail.com>
2019-08-26 14:17:23 +0000

    Add files from last week

---
+++ HelloName.java
@@ -0,0 +1,8 @@
+public class HelloName
+{
+    public static void main(String[] args)
+    {
+        String LastName = "Mason";
+        System.out.println("Hello, " + lastName);
+    }
+}
---
+++ booboo.java
@@ -0,0 +1,7 @@
+public class booboo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("im hungry");
+    }
+}
