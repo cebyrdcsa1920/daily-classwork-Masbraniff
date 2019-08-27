ea48f0356b92958a59e004c64b7a3da7c62ddc3a
Masbraniff <masontyler272@gmail.com>
2019-08-27 14:12:45 +0000

    i dont think that this stupid thing matters but i guess we will see

--- week3/Student.java
+++ week3/Student.java
@@ -1,3 +1,21 @@
 public class Student
-public class Student
-public class Student
+{
+    private double gpa;
+    private String studentID;
+    private String name;
+    private int age;
+
+    public Student(String name, String studentID, int age)
+    {
+        this.gpa = 0.0;
+        this.name = name;
+        this.studentID = studentID;
+        this.age = age;
+    }
+    public static void main(String[] args)
+    {
+        Student mason = new Student("Mason","690905",17);
+        System.out.println(mason);
+    }
+
+}
