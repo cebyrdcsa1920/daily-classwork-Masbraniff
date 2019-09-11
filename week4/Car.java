f6dfb1020b0b459e514297e1941f99aed43ea2f5
Masbraniff <masontyler272@gmail.com>
2019-09-11 13:38:22 +0000

    Added gas to the tank

---
+++ week4/Car.java
@@ -0,0 +1,45 @@
+public class House
+{
+    private  String Build;
+    private Color Color;
+    private int numOccupants;
+    private double amountOfAcres;
+
+    public House()
+    {
+        build = "Two Story";
+        color = Color.RED;
+        numOccupants = 3;
+        amountOfAcres = 12.5;
+    }
+    public static void main(String[] args)
+    {
+       return "Its my house i can do what i want to";
+    }
+    public void drive(double distance)
+    {
+        amountOfGas -= distance / mpg;
+    }
+
+       /**
+        * Gets the amount of gas in the tank
+        *
+        * @return amount of gas
+        * /
+
+        public double getAmountOfGas()
+        {
+            return amountOfGas;
+        }
+
+    /**
+     * Fills up the gas tank
+     *
+     * @param amount of
+     */
+    public void fillUpGasTank(double amount)
+    {
+        amountOfGas += amount;
+    }
+
+}
